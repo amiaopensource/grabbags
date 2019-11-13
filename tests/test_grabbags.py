338aa6e562cf233f0dd34624604dd6e1ac1c41be
henryborchers <henryborchers@yahoo.com>
2019-11-13 14:48:14 -0500

    added is_system_file

--- tests/test_grabbags.py
+++ tests/test_grabbags.py
@@ -1,9 +1,11 @@
+import os
+
+import grabbags.utils
+import pytest
+
+
 def test_batch_validate():
     # provide a directory
-    pass
-
-
-def test_validate_is_bag():
     pass
 
 
@@ -12,5 +14,28 @@ def test_bulk_creation():
 
 
 def test_delete_annoying_files():
-    # Delete ds_store. etc
-    pass
+    sample_file = os.path.join(os.getcwd(), "dummy.txt")
+
+    assert grabbags.utils.is_system_file(sample_file) is False
+
+
+@pytest.mark.parametrize("entry,", [
+        "._somethingsomething",
+        "._f",
+        "._~",
+        "._/something",
+    ])
+def test_apple_doubles_valid(entry):
+    assert grabbags.utils.is_system_file(entry) is True
+
+
+@pytest.mark.parametrize("entry,", [
+    ".s",
+    "asdfa.yes",
+    "_.fdjlkdfj",
+    "something._something",
+    "fdjklfj ._ dklj",
+    ".dj_djfklj"
+])
+def test_not_apple_doubles(entry):
+    assert grabbags.utils.is_system_file(entry) is False
