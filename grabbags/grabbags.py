98f33833b0df90cd1e7aba813fc8749272eec5d3
Milo Thiesen <mailethiesen@gmail.com>
2020-01-15 11:19:09 -0500

    Update grabbags.py
    removed continue statements

--- grabbags/grabbags.py
+++ grabbags/grabbags.py
@@ -180,7 +180,6 @@ def _configure_logging(opts):
 def validate_bag(bag_dir, args):
     if not is_bag(bag_dir.path):
         LOGGER.warn(_("%s is not a bag. Skipped."), bag_dir.path)
-        # continue
 
     bag = bagit.Bag(bag_dir.path)
     # validate throws a BagError or BagValidationError
@@ -203,7 +202,6 @@ def validate_bag(bag_dir, args):
 def clean_bag(bag_dir):
     if not is_bag(bag_dir.path):
         LOGGER.warn(_("%s is not a bag. Not cleaning."), bag_dir.path)
-        # continue
 
     bag = bagit.Bag(bag_dir.path)
     if bag.compare_manifests_with_fs()[1]:
@@ -218,7 +216,6 @@ def clean_bag(bag_dir):
 def make_bag(bag_dir, args):
     if is_bag(bag_dir.path):
         LOGGER.warn(_("%s is already a bag. Skipped."), bag_dir.path)
-        # continue
 
     if args.no_system_files is True:
         LOGGER.info(_("Cleaning %s of system files"), bag_dir.path)
